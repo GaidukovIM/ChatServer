0b5baf471a7aa785d84042e61298cf54a5c1b68a
GaidukovIM <ivan.gaydukov2005@gmail.com>
2022-12-08 23:16:14 +0300

    Добавьте файлы проекта.

---
+++ ChatServer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+namespace ChatServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
